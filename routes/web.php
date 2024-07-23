eebae6229e4e732176338203818775219fe953a6
erwin dwi s <erwin@waki.mobi>
2024-07-23 17:09:44 +0700

    feat: add middleware guest

--- routes/web.php
+++ routes/web.php
@@ -4,20 +4,12 @@ use App\Http\Controllers\AuthController;
 use App\Http\Controllers\InvoiceController;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
-Route::view('/signin', 'pages.signin')->name('signin');
-Route::view('/signup', 'pages.signup')->name('signup');
-Route::post('signup', [AuthController::class, 'signup'])->name('post_signup');
-Route::post('signin', [AuthController::class, 'signin'])->name('post_signin');
+Route::middleware('guest:web')->group(function () {
+	Route::view('/signin', 'pages.signin')->name('signin');
+	Route::view('/signup', 'pages.signup')->name('signup');
+	Route::post('signup', [AuthController::class, 'signup'])->name('post_signup');
+	Route::post('signin', [AuthController::class, 'signin'])->name('post_signin');
+});
 
 // with session
 Route::get('/', [InvoiceController::class, 'view_invoice'])->name('view_invoice')->middleware("auth");
